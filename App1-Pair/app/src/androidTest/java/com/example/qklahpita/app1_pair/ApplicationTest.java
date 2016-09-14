4988ef147e4946cfb64f2a484874827b7473e22f
Qk Lahpita <lpita.91@gmail.com>
2016-09-14 13:21:34 +0700

    1

---
+++ App1-Pair/app/src/androidTest/java/com/example/qklahpita/app1_pair/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.qklahpita.app1_pair;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ App1-Pair/app/src/main/java/com/example/qklahpita/app1_pair/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.qklahpita.app1_pair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
